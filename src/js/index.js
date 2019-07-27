50ff90fa6236006cab7b9271a4cfdcfc0d863b2a
Jarek Troyer <jarektroyer@gmail.com>
2019-07-27 15:45:04 -0400

    Temporary publishing

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-const map = require('classes/map.js')
-
-let args = {
-    'passionContainerId': 'mapWrap',
-    'passionNavClass': '.pickYourPassionOpt'
-}
-const passionMap = map(args)
-
-console.log(passionMap)
