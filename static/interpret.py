03f3deada828fa7aa7725ba1de90ef1879ece9ca
Jarek Troyer <jarektroyer@gmail.com>
2019-08-01 20:28:40 -0400

    Added geocoding to passion preprocessing

--- static/interpret.py
+++ static/interpret.py
@@ -1,6 +1,7 @@
-import os, sys, re, csv, json
+import os, sys, re, csv, json, requests, time
 
 class YapDataReadier:
+    geocodingURL = "https://us1.locationiq.com/v1/search.php"
     orgIndex = {
         "Reproductive Justice": "repJustic",
         "Sexual & Domestic Violence": "sVil",
@@ -23,26 +24,56 @@ class YapDataReadier:
         "immAccess": {},
         "lgbtq": {}
     }
-
     passionIndex = {}
 
     def splitAddress(self, address, out):
-        addrList = re.split(r"(\,|-)", address)
+        onlyCity = re.findall(r'(?<=\\N\/A\s\()\w+', address)
 
-        if len(addrList) == 5:
-            out["addr0"] = False
-            out["addr1"] = addrList[0]
-            out["addr2"] = addrList[-3] + addrList[-2] + addrList[-1]
-
-        
-        elif len(addrList) == 7:
-            out["addr0"] = addrList[0]
-            out["addr1"] = addrList[2][1:]
-            out["addr2"] = addrList[-3] + addrList[-2] + addrList[-1]
-        
+        if onlyCity: out["addr1"] = onlyCity[0]
+            
         else:
-            out["addr0"] = out["addr1"] = out["addr2"] = False
+            addrList = re.split(r"(\,|-)", address)
+
+            if len(addrList) == 5:
+                out["addr0"] = False
+                out["addr1"] = addrList[0]
+                out["addr2"] = addrList[-3] + addrList[-2] + addrList[-1]
+
+            
+            elif len(addrList) == 7:
+                out["addr0"] = addrList[0]
+                out["addr1"] = addrList[2][1:]
+                out["addr2"] = addrList[-3] + addrList[-2] + addrList[-1]
+            
+            else:
+                out["addr0"] = out["addr1"] = out["addr2"] = False
     
+    def getAddress(self, query):
+        data = {
+            'key': '3da6488b6abe32',
+            'q': query,
+            'format': 'json'
+        }
+
+        resp = json.loads((requests.get(self.geocodingURL, params = data)).text)
+
+        lat = resp[0]['lat']
+        lon = resp[0]['lon']
+
+        return lat, lon
+
+    def getPid(self, attrs):
+        pId = ''
+
+        if attrs['phone'] != 'N/A':
+            pId =  pId.join(re.findall(r'\d+', attrs['phone']))
+        else:
+            pId += 'NA-'+pId.join([w[0:2] for w in attrs["name"].split(' ')])
+
+        attrs["id"] = pId
+
+        return pId
+
     def parseSource(self, data):
         next(data)
 
@@ -50,15 +81,27 @@ class YapDataReadier:
         for row in data:
             targetIndex = self.orgs[self.orgIndex[row[1]]]
 
+            toQuery = re.findall(r'(?<=-\s).+', row[2])
+            toQuery = toQuery if toQuery else re.findall(r'(?<=\\N\/A\s\()\w+', row[2])
+            toQuery = toQuery if toQuery else row[2]
+            
+            coords = self.getAddress(toQuery)
+            
+
             attrs = {
                 "name": row[0],
                 "link": row[4],
-                "phone": row[3]
+                "phone": row[3],
+                "coords": {"lat": coords[0], "lon": coords[1]}
             }
-
             self.splitAddress(row[2], attrs)
+
+            pId = self.getPid(attrs)#re.findall(row[3])
+            # attrs["id"] = pId
+
+            targetIndex[pId] = attrs
             
-            targetIndex[row[3]] = attrs
+            time.sleep(.5) # Our geocoding API has a request rest period
 
     def makeKeyIndex(self, orgIndex = False):
         orgIndex = orgIndex if orgIndex else self.orgIndex
@@ -75,7 +118,7 @@ class YapDataReadier:
                 orgInfo = (self.orgs[cat])[org]
 
                 if orgInfo: 
-                    orgList.append( { "name": orgInfo['name'], "id": orgInfo['phone'] } )
+                    orgList.append( { "name": orgInfo['name'], "id": orgInfo['id'] } )
 
             if not len(orgList):
                 orgList.append({"name": "test", "id": "_test"})
