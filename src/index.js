ea0db54ead1a4e8d2f721746fe084d8a25f26255
Jarek Troyer <jarektroyer@gmail.com>
2019-08-23 18:13:23 -0400

    Styling progress

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ let args = {
 
     'mapArgs': {
         'containerID': 'mapWrap',
-        'layerSrc': 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+        'layerSrc': 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         'layerArgs': {
-            attribution: '<a href="https://www.esri.com/en-us/home">Esri</a>',
+            attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             minZoom: 7
         },
         zoom: 7,
